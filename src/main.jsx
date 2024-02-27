cbf3f14c920351ad544cebef354c155a315e5fce
Yevhen Vriashnyk <jackavryashnik@gmail.com>
2024-02-27 23:27:05 +0200

    create file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'modern-normalize';
-import App from './App.jsx';
+import App from './components/App/App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
